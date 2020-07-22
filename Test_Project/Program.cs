f2aedfe9aa0f890f6e9300c4906230c74587de18
Daniel Fonseca <daniel_mf@live.com>
2020-07-22 08:09:19 -0300

    Added a Good Morning message

--- Test_Project/Program.cs
+++ Test_Project/Program.cs
@@ -4,6 +4,7 @@ namespace Test_Project {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning!");
         }
     }
 }
